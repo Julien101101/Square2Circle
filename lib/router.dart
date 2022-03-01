0c0c34d5510d5d0bef7cc2388e368514cd503032
Julien Savary <Juliensavary2001@gmail.com>
2022-03-01 10:21:59 -0800

    initial setup

--- lib/router.dart
+++ lib/router.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
   }
---
+++ lib/screens/widgets/questions.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../widgets/rounded_input_field.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:my_app/screens/widgets/rounded_input_field.dart';
+import '../../viewmodels/questionsviewmodel.dart';
+
+class Questions extends StatefulWidget {
+  final String? questionText;
+  Questions({Key? key, this.questionText}) : super(key: key);
+
+  @override
+  QuestionsState createState() => QuestionsState();
+}
+
+class QuestionsState extends State<Questions> {
+  @override
+  Widget build(BuildContext context) {
+    QuestionsViewModel questionsViewModel = context.watch<QuestionsViewModel>();
+    int sides = 0;
+
+    return Container(
+      child: RoundedInputField(
+        textInputType: TextInputType.text,
+        hintText: widget.questionText,
+        onChanged: (value) {
+          if (value == "yes") {
+            sides++;
+          } else {
+            sides--;
+          }
+          questionsViewModel.savedResponses(sides, "user");
+        },
+      ),
+    );
+  }
+}
