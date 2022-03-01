0aecb0718b86a276cf1193fb2ff5bae94c214c60
Julien Savary <Juliensavary2001@gmail.com>
2022-03-01 11:52:20 -0800

    designing widget

--- lib/screens/widgets/questions.dart
+++ lib/screens/widgets/questions.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import '../widgets/rounded_input_field.dart';
+import 'package:my_app/screens/widgets/rounded_input_field.dart';
+import 'package:my_app/utilities/appColors.dart';
+import '../../viewmodels/questionsviewmodel.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:my_app/screens/widgets/rounded_input_field.dart';
-import '../../viewmodels/questionsviewmodel.dart';
 
 class Questions extends StatefulWidget {
   final String? questionText;
@@ -14,24 +14,73 @@ class Questions extends StatefulWidget {
 }
 
 class QuestionsState extends State<Questions> {
+  bool _isEditingText = false;
+  TextEditingController? _editingController;
+  String initialText = "Initial Text";
+  @override
+  void initState() {
+    super.initState();
+    _editingController = TextEditingController(text: initialText);
+  }
+
+  @override
+  void dispose() {
+    _editingController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     QuestionsViewModel questionsViewModel = context.watch<QuestionsViewModel>();
     int sides = 0;
 
     return Container(
-      child: RoundedInputField(
-        textInputType: TextInputType.text,
-        hintText: widget.questionText,
-        onChanged: (value) {
-          if (value == "yes") {
-            sides++;
-          } else {
-            sides--;
-          }
-          questionsViewModel.savedResponses(sides, "user");
-        },
-      ),
-    );
+        color: AppColors.backPink,
+        child: Center(
+            child: Container(
+          color: AppColors.insideGreen,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5), //color of shadow
+                spreadRadius: 5, //spread radius
+                blurRadius: 7, // blur radius
+                offset: Offset(0, 2), // changes position of shadow
+                //first paramerter of offset is left-right
+                //second parameter is top to down
+              ),
+              //you can set more BoxShadow() here
+            ],
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  widget.questionText!,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12.0,
+                  ),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: TextField(
+                    controller: _editingController,
+                    autofocus: true,
+                    onSubmitted: (value) {
+                      initialText = value;
+                      _isEditingText = false;
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ))
+            ],
+          ),
+        )));
   }
 }
---
+++ lib/utilities/appColors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backPink = Color(0xFFE77577);
+  static const insideGreen = Color(0xFFB2FF59);
+}
