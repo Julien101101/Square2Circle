4c9a93cb3a6f6628040a40d96acf59db7ac85e3f
wolf <billodida420@gmail.com>
2022-03-08 20:46:10 +0300

    fixing bugs

--- lib/router.dart
+++ lib/router.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/screens/homePage.dart';
 import 'package:my_app/screens/introScreen.dart';
 
 Future<Route> generateRoute(RouteSettings settings) async {
   switch (settings.name) {
+    case 'home':
+    return MaterialPageRoute(builder: (context) => const HomePage());
     default:
-      return MaterialPageRoute(builder: (context) => const IntroScreen(key: Key('intro'),));;
+      return MaterialPageRoute(builder: (context) => const IntroScreen(key: Key('intro'),));
   }
 }
